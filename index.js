1c021ad1fcf7d2bccc5bf95ffa8f53a15a5d1ec1
Fernando Daciuk <f.daciuk@gmail.com>
2016-03-11 01:40:56 -0300

    Add cli lib

--- index.js
+++ index.js
@@ -1,2 +1,80 @@
+#!/usr/bin/env node
+
 'use strict'
-console.log('hmh-cli')
+
+const hmh = require('hmh')
+const pkg = require('./package.json')
+const hmhPkg = require('hmh/package.json')
+const args = process.argv.slice(2)
+
+const help = `
+  Usage
+    $ hmh <option> <time> <output>
+
+  Options
+    -h, --help                      Show this help
+    -v, --version                   Show hmh and hmh-cli version
+    --sum <time>                    Calculate hours, summing time spaces
+    --sub <time>                    Calculate hours, subtracting time spaces
+    --diff <firstTime> <SecondTime> Calculate the difference between two time spaces
+    --div <time> <divisor>          Divide a time space into a number, passed as 'divisor'.
+
+  Example
+    $ hmh --sum 10m 20m 30m 1h 25m
+    2h 25m
+
+    $ hmh --sub 10h 20m 1m
+    9h 39m
+
+    $ hmh --diff "10h 30m" "12h"
+    1h 30m
+
+    $ hmh --diff 10h30m 12h
+    1h 30m
+
+    $ hmh --div 9h 3
+    3h
+`
+
+const version = `
+  hmh: ${hmhPkg.version}
+  hmh-cli: ${pkg.version}
+`
+
+const getOutput = (argv) => {
+  return argv[argv.length - 1]
+}
+
+const getMethod = (argv) => {
+  return argv[0].replace(/--?/, '')
+}
+
+const getValues = (argv) => {
+  if (getMethod(argv) === 'diff' || getMethod(argv) === 'div') {
+    return argv.slice(1)
+  }
+  return argv.filter((value) => value.match(/\d+[hm]/))
+}
+
+const method = getMethod(args)
+const values = getValues(args)
+const output = getOutput(args)
+
+try {
+  switch (method) {
+    case 'v':
+    case 'version':
+      console.log(version)
+      break
+
+    case 'diff':
+    case 'div':
+      console.log(hmh[method](values[0], values[1], output))
+      break
+
+    default:
+      console.log(hmh[method](values, output))
+  }
+} catch (e) {
+  console.log(help)
+}
